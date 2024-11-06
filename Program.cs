c08aef7ff009c20f0b311d27631609ce91ddc2ed
Andrea Iannì <andrea.ianni@avbo.it>
2024-11-06 11:10:36 +0100

    Aggiunti file del progetto e configurazione innosetup

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+string veyonPath = @"c:\Program Files\Veyon";
+string keyFolder = "keys";
+
+if (args.Length > 0)
+	keyFolder = args[0];
+if (args.Length > 1)
+	veyonPath = args[1];
+
+using (Process configTool = new Process())
+{
+	configTool.StartInfo.FileName = Path.Combine(veyonPath, "veyon-cli.exe");
+
+	configTool.StartInfo.Arguments = $"config set Authentication/PrivateKeyBaseDir \"%GLOBALAPPDATA%\\{keyFolder}\\private\"";
+	configTool.StartInfo.UseShellExecute = true;
+	configTool.StartInfo.Verb = "runas";
+	configTool.Start();
+	configTool.WaitForExit();
+}
+
+//using (Process configTool = new Process())
+//{
+//	configTool.StartInfo.FileName = Path.Combine(veyonPath, "veyon-cli.exe");
+
+//	configTool.StartInfo.Arguments = $"config get Authentication/PrivateKeyBaseDir";
+//	configTool.Start();
+//	configTool.WaitForExit();
+//}
+
+using (Process configTool = new Process())
+{
+	configTool.StartInfo.FileName = Path.Combine(veyonPath, "veyon-master.exe");
+	configTool.Start();
+}
